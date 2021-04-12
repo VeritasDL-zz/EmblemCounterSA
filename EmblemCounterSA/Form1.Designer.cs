82ab76f89d6e2cd9321acc7c97384e776f28ec36
Veritas <VeritasDL@users.noreply.github.com>
2021-04-11 21:51:34 -0400

    Upload
    Not entirely sure how finished or polished this is
    from 2015

---
+++ EmblemCounterSA/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace EmblemCounterSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.EmblemBox1 = new System.Windows.Forms.PictureBox();
+            this.emblemcountlabel = new System.Windows.Forms.Label();
+            this.emblemnum1 = new System.Windows.Forms.PictureBox();
+            this.emblemnum2 = new System.Windows.Forms.PictureBox();
+            this.colonimagebox = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.emblemnum3 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emblemGetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sADXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sA2BToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.EmblemBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emblemnum1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emblemnum2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colonimagebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emblemnum3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // EmblemBox1
+            // 
+            this.EmblemBox1.Image = ((System.Drawing.Image)(resources.GetObject("EmblemBox1.Image")));
+            this.EmblemBox1.Location = new System.Drawing.Point(13, 12);
+            this.EmblemBox1.Name = "EmblemBox1";
+            this.EmblemBox1.Size = new System.Drawing.Size(174, 112);
+            this.EmblemBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.EmblemBox1.TabIndex = 1;
+            this.EmblemBox1.TabStop = false;
+            // 
+            // emblemcountlabel
+            // 
+            this.emblemcountlabel.Font = new System.Drawing.Font("Gaslight", 21.75F, System.Drawing.FontStyle.Bold);
+            this.emblemcountlabel.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.emblemcountlabel.Location = new System.Drawing.Point(193, 53);
+            this.emblemcountlabel.Name = "emblemcountlabel";
+            this.emblemcountlabel.Size = new System.Drawing.Size(199, 31);
+            this.emblemcountlabel.TabIndex = 2;
+            this.emblemcountlabel.Text = "Emblem Count";
+            // 
+            // emblemnum1
+            // 
+            this.emblemnum1.Image = ((System.Drawing.Image)(resources.GetObject("emblemnum1.Image")));
+            this.emblemnum1.Location = new System.Drawing.Point(390, 51);
+            this.emblemnum1.Name = "emblemnum1";
+            this.emblemnum1.Size = new System.Drawing.Size(35, 35);
+            this.emblemnum1.TabIndex = 3;
+            this.emblemnum1.TabStop = false;
+            // 
+            // emblemnum2
+            // 
+            this.emblemnum2.Image = ((System.Drawing.Image)(resources.GetObject("emblemnum2.Image")));
+            this.emblemnum2.Location = new System.Drawing.Point(427, 51);
+            this.emblemnum2.Name = "emblemnum2";
+            this.emblemnum2.Size = new System.Drawing.Size(31, 35);
+            this.emblemnum2.TabIndex = 4;
+            this.emblemnum2.TabStop = false;
+            // 
+            // colonimagebox
+            // 
+            this.colonimagebox.Image = ((System.Drawing.Image)(resources.GetObject("colonimagebox.Image")));
+            this.colonimagebox.Location = new System.Drawing.Point(364, 55);
+            this.colonimagebox.Name = "colonimagebox";
+            this.colonimagebox.Size = new System.Drawing.Size(24, 31);
+            this.colonimagebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.colonimagebox.TabIndex = 5;
+            this.colonimagebox.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "snum_0.png");
+            this.imageList1.Images.SetKeyName(1, "snum_1.png");
+            this.imageList1.Images.SetKeyName(2, "snum_2.png");
+            this.imageList1.Images.SetKeyName(3, "snum_3.png");
+            this.imageList1.Images.SetKeyName(4, "snum_4.png");
+            this.imageList1.Images.SetKeyName(5, "snum_5.png");
+            this.imageList1.Images.SetKeyName(6, "snum_6.png");
+            this.imageList1.Images.SetKeyName(7, "snum_7.png");
+            this.imageList1.Images.SetKeyName(8, "snum_8.png");
+            this.imageList1.Images.SetKeyName(9, "snum_9.png");
+            this.imageList1.Images.SetKeyName(10, "snum_co.png");
+            // 
+            // emblemnum3
+            // 
+            this.emblemnum3.Image = ((System.Drawing.Image)(resources.GetObject("emblemnum3.Image")));
+            this.emblemnum3.Location = new System.Drawing.Point(391, 51);
+            this.emblemnum3.Name = "emblemnum3";
+            this.emblemnum3.Size = new System.Drawing.Size(34, 35);
+            this.emblemnum3.TabIndex = 6;
+            this.emblemnum3.TabStop = false;
+            this.emblemnum3.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(496, 100);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(42, 24);
+            this.menuStrip1.Stretch = false;
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "Jewish Strip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.emblemGetToolStripMenuItem,
+            this.modeToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Gaslight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // emblemGetToolStripMenuItem
+            // 
+            this.emblemGetToolStripMenuItem.Name = "emblemGetToolStripMenuItem";
+            this.emblemGetToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.G)));
+            this.emblemGetToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.emblemGetToolStripMenuItem.Text = "Emblem Get";
+            this.emblemGetToolStripMenuItem.Click += new System.EventHandler(this.emblemGetToolStripMenuItem_Click);
+            // 
+            // modeToolStripMenuItem
+            // 
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sADXToolStripMenuItem,
+            this.sA2BToolStripMenuItem});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            this.modeToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.modeToolStripMenuItem.Text = "Mode";
+            // 
+            // sADXToolStripMenuItem
+            // 
+            this.sADXToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.sADXToolStripMenuItem.Name = "sADXToolStripMenuItem";
+            this.sADXToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.sADXToolStripMenuItem.Text = "SADX";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Checked = true;
+            this.toolStripMenuItem2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(100, 22);
+            this.toolStripMenuItem2.Text = "130";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(100, 22);
+            this.toolStripMenuItem3.Text = "20";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // sA2BToolStripMenuItem
+            // 
+            this.sA2BToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4});
+            this.sA2BToolStripMenuItem.Name = "sA2BToolStripMenuItem";
+            this.sA2BToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.sA2BToolStripMenuItem.Text = "SA2B";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(100, 22);
+            this.toolStripMenuItem4.Text = "180";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "emblem.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 132);
+            this.ControlBox = false;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.emblemnum3);
+            this.Controls.Add(this.colonimagebox);
+            this.Controls.Add(this.emblemnum2);
+            this.Controls.Add(this.emblemnum1);
+            this.Controls.Add(this.emblemcountlabel);
+            this.Controls.Add(this.EmblemBox1);
+            this.Font = new System.Drawing.Font("Gaslight", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(23, 60, 23, 20);
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Black;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmblemBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emblemnum1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emblemnum2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colonimagebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emblemnum3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox EmblemBox1;
+        private System.Windows.Forms.Label emblemcountlabel;
+        private System.Windows.Forms.PictureBox emblemnum1;
+        private System.Windows.Forms.PictureBox emblemnum2;
+        private System.Windows.Forms.PictureBox colonimagebox;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox emblemnum3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emblemGetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sADXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem sA2BToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ EmblemCounterSA/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+
+
+namespace EmblemCounterSA
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool sadx130;
+        bool sadx20;
+        bool sa2b180;
+        int emblemtotal;
+        int emblemnumber1;
+        int emblemnumber2;
+        int emblemnumber3;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emblemGetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (emblemnumber1 == 9)
+            {
+                emblemnumber2++;
+                emblemtotal++;
+                emblemnumber1 = 0;
+                emblemnum2.Image = imageList1.Images[emblemnumber1];
+                emblemnum1.Image = imageList1.Images[emblemnumber2];
+
+            }
+            else
+            {
+                emblemnumber1++;
+                emblemtotal++;
+                emblemnum2.Image = imageList1.Images[emblemnumber1];
+            }
+            if (emblemnumber1 == 9 && emblemnumber2 == 9)
+            {
+                emblemnum3.Visible = true;
+                emblemnumber1 = 0;
+                emblemnumber2 = 0;
+                emblemnumber3++;
+                emblemtotal++;
+                emblemnum3.Image = imageList1.Images[emblemnumber3];
+                emblemnum2.Image = imageList1.Images[emblemnumber1];
+                emblemnum1.Image = imageList1.Images[emblemnumber2];
+                emblemnum1.Location = new System.Drawing.Point(447, 51);
+                emblemnum2.Location = new System.Drawing.Point(503, 51);
+            }
+            if (sadx130 == true && emblemtotal == 130)
+            {
+                MessageBox.Show("Congrats", "WINNER", System.Windows.Forms.MessageBoxButtons.OK);
+            }
+            if (sadx20 == true && emblemtotal == 20)
+            {
+                MessageBox.Show("Congrats", "WINNER", System.Windows.Forms.MessageBoxButtons.OK);
+            }
+            if (sa2b180 == true && emblemtotal == 180)
+            {
+                MessageBox.Show("Congrats", "WINNER", System.Windows.Forms.MessageBoxButtons.OK);
+            }
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            emblemnumber1 = 0;
+            emblemnumber2 = 0;
+            emblemnumber3 = 0;
+            emblemtotal = 0;
+            emblemnum3.Visible = false;
+            emblemnum3.Image = imageList1.Images[emblemnumber3];
+            emblemnum2.Image = imageList1.Images[emblemnumber1];
+            emblemnum1.Image = imageList1.Images[emblemnumber2];
+            emblemnum1.Location = new System.Drawing.Point(391, 51);
+            emblemnum2.Location = new System.Drawing.Point(427, 51);
+            
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+                toolStripMenuItem2.Checked = true;
+                toolStripMenuItem3.Checked = false;
+                toolStripMenuItem4.Checked = false;
+                sadx130 = true;
+                sadx20 = false;
+                sa2b180 = false;
+            
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+                toolStripMenuItem3.Checked = true;
+                toolStripMenuItem2.Checked = false;
+                toolStripMenuItem4.Checked = false;
+                sadx20 = true;
+                sadx130 = false;
+                sa2b180 = false;
+            
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+                toolStripMenuItem4.Checked = true;
+                toolStripMenuItem2.Checked = false;
+                toolStripMenuItem3.Checked = false;
+                sadx20 = false;
+                sadx130 = false;
+                sa2b180 = true;
+            
+        }
+
+    }
+}
